faf9fe13c4c519ff8e6bfa7b2703856ceba85636
Swagrzyk <mswagrzyk@interia.pl>
2023-06-11 14:22:03 +0200

    currency_conversion_2

---
+++ src/main/java/com/example/currency/calculator/CurrencyController.java
@@ -0,0 +1,36 @@
+package com.example.currency.calculator;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CurrencyController {
+    private static final String NBP_API_URL = "http://api.nbp.pl/api/exchangerates/tables/a/";
+
+    @GetMapping("/convert")
+    public double convertCurrency(
+            @RequestParam("amount") double amount,
+            @RequestParam("from") String fromCurrency,
+            @RequestParam("to") String toCurrency
+    ) {
+        double fromRate = getExchangeRate(fromCurrency);
+        double toRate = getExchangeRate(toCurrency);
+
+        double convertedAmount = (amount / fromRate) * toRate;
+        return convertedAmount;
+    }
+
+    private double getExchangeRate(String currencyCode) {
+        String apiUrl = NBP_API_URL + currencyCode + "/?format=json";
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<ExchangeRateResponse> responseEntity = restTemplate.getForEntity(apiUrl, ExchangeRateResponse.class);
+        ExchangeRateResponse response = responseEntity.getBody();
+        if (response != null) {
+            return response.getRates().get(0).getMid();
+        }
+        return 0.0;
+    }
+}
+
---
+++ src/main/java/com/example/currency/calculator/ExchangeRateResponse.java
@@ -0,0 +1,37 @@
+package com.example.currency.calculator;
+
+import java.util.List;
+
+public class ExchangeRateResponse {
+    private String currency;
+    private String code;
+    private List<Rate> rates;
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public List<Rate> getRates() {
+        return rates;
+    }
+
+    public void setRates(List<Rate> rates) {
+        this.rates = rates;
+    }
+}
+
+
+
+
